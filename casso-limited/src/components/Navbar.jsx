6b2b34e2e79da38de4a0c55f19216369209416c4
Mike <michalkaso@yahoo.com>
2022-10-18 20:52:40 +0100

    Navbar

--- casso-limited/src/components/Navbar.jsx
+++ casso-limited/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import {useState} from 'react';
+import { close, logo, menu} from '../assets';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='w-full flex py-6 
+    justify-between items-center navbar'>
+
+    </nav>
   )
 }
 
