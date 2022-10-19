46e2a72bca973ca9f8c5841c21d2677b2c94debf
Mike <michalkaso@yahoo.com>
2022-10-19 19:54:05 +0100

    components Final

--- casso-limited/src/App.jsx
+++ casso-limited/src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import styles from './style';
+import  styles from './style.js';
+
 
 import { Navbar, Hero, Stats, Business, Billing, CardDeal, 
   Testimonials, Clients, CTA, Footer, } from './components';
 
 
 const App = () => (
+
+  
   <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter} `}>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-    {/* Main Part of App */}
+    
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
@@ -21,27 +23,28 @@ const App = () => (
     </div>
 
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        <Stats /> 
-        <Business /> 
-        <Billing /> 
-        <CardDeal /> 
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
         <Testimonials />
-        <Clients /> 
-        <CTA /> 
+        <Clients />
+        <CTA />
         <Footer />
       </div>
     </div>
-
-
   </div>
 
+
+  
+
   
   
 
 
-)
+);
  
 
 export default App
